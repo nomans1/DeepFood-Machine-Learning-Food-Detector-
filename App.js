6a93295a679fc46000308f2084277c81e0216f38
Noman Sanaullah <nomansanaullah@Nomans-MacBook-Pro.local>
2019-03-17 20:33:23 +0000

    React Native app shell

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import CameraComponent from "./Components/Camera";
+
+export default class App extends React.Component {
+  render() {
+    return <CameraComponent />;
+  }
+}
